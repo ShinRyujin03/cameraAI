82cd4380d3c6ee93a42b853d67a4a7a35b6b31d2
ShinRyujin03 <duonghailong03@gmail.com>
2023-08-16 11:49:56 +0700

    .

---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+class Config:
+    key = os.urandom(24)
+    img_folder = '/Users/macbookairm1/Desktop/Viettel/CameraAI/output/captured_images'
+    vid_filename = '/Users/macbookairm1/Desktop/Viettel/CameraAI/output/captured_video.mp4'
+    backlog_filename = '/Users/macbookairm1/Desktop/Viettel/CameraAI/output/backlog.txt'
